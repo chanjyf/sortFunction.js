6b47d98ef216a11647c0e8473d7744916357cc95
jamesjchan <61301770+chanjyf@users.noreply.github.com>
2021-03-26 13:54:27 -0300

    Update sort.js

--- sort.js
+++ sort.js
@@ -1,15 +1,15 @@
 const arr = [3, 9, 6, 5, 7, 1, 2];
 
-function ordenaArrayV2(arr) {
+function sortArray(arr) {
     for (let i = 0; i < arr.length; i++) {
         for (let j = i + 1; j < arr.length; j++) {
             if (arr[i] > arr[j]) {
-                const indexMenor = arr[j];
+                const indexMinor = arr[j];
                 arr[j] = arr[i];
-                arr[i] = indexMenor;
+                arr[i] = indexMinor;
             }
         }
     }
     return arr;
 }
-console.log(ordenaArrayV2(arr));
+console.log(sortArray(arr));
